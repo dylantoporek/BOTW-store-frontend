0dbfbee824344748f8bbd9160366c6d9917dc013
DylanToporek <dylantoporek@gmail.com>
2022-01-05 12:20:25 -0500

    fetch from backend set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [stores, setStores] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/stores")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach((result) => {
+        fetchStoreData(result.id)
+      })
+    })
+  }, [])
+
+  function fetchStoreData(id){
+    fetch(`http://localhost:9292/stores/${id}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  
   return (
     <div>
     </div>
